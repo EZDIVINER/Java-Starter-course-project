75b08036040a68f562312803819d07f16468b0fd
Eugen <ezdiviner@gmail.com>
2022-12-04 12:31:04 +0200

    Task 1 Done

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,7 +1,19 @@
 package Task1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Enter a number");
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        System.out.println(function(num));
+
+    }
+    static int function(int num){
+        int sum = 0;
+        for ( ; num > 0; num /= 10)
+            sum += num % 10;
+        return sum;
     }
 }
